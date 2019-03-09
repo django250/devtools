a6fce377682cb26a02d7531e79266427eb895ae0
RickZ <rick-zw@hotmail.com>
2019-03-09 16:47:27 +0100

    Added unit testing

--- node_server/src/customer.js
+++ node_server/src/customer.js
@@ -4,22 +4,24 @@ var self = module.exports = {
     return {
       single_name: 'Rick',
       last_name: 'Zwijnenberg',
+      age: 23,
     }
   },
 
   getter: {
     getFullName(){
-      console.log('Get full name');
       return self.data().single_name + ' ' + self.data().last_name;
     },
     getFirstName(){
-      console.log('Get first name');
       return self.data().single_name;
     },
     getLastName(){
-      console.log('Get last name');
       return self.data().last_name;
     },
+    getAge(){
+      return self.data().age;
+    }
+
   },
 
 }
